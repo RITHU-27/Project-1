242e121590672a5fb1e5ee38cb0475552e65a3fe
RITHU-27 <27rithu10@gmail.com>
2026-06-18 09:56:26 +0530

    Updated URL Render

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: "https://project-1-dwnq.vercel.app/" }));
+app.use(cors({ origin: "https://project-1-dwnq-git-main-rithanyaa-v-s-projects.vercel.app/" }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
